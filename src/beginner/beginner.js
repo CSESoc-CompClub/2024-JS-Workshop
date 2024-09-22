3bf199f8f3dc11c19f86fb7cdf05cff166ae0265
Anna Zhang <z5481945@ad.unsw.edu.au>
2024-09-22 20:28:55 +1000

    tests made for type of, number methods

--- src/beginner/beginner.js
+++ src/beginner/beginner.js
@@ -1,3 +1,26 @@
 export function sum(a, b) {
   return a + b;
 }
+
+// TODO:
+// type of
+// numbers
+// number methods (toString, parseInt)
+// Number properties (NaN)
+// operators and arithmetic
+
+export function typeOf(pumpkin) {
+  return typeof pumpkin;
+}
+
+export function spookyString(thirteen) {
+  return thirteen.toString();
+}
+
+export function ghoulishNumbers(scarecrow) {
+  return parseInt(scarecrow);
+}
+
+// export function hauntedNumber(ghost) {
+//   return ghost.NaN;
+// }
