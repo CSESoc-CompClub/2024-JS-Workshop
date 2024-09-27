fa95c64e4a8265556c3ac63e01677b8138cbcafa
Nicole Chun <19chunnw3@gmail.com>
2024-09-27 15:35:11 +1000

    advanced

---
+++ src/advanced/advanced.js
@@ -0,0 +1,30 @@
+/*
+#************************************#
+#   COMPCLUB Spring Workshops 2024   #
+#    Introduction to Javascript      #
+#************************************#
+
+By: <YOUR-NAME-HERE>
+Written On: <DATE-HERE>
+
+*/
+// ****************************************************************************
+// PART A: Costume rizzmaxxing
+// ****************************************************************************
+export function rizzMaxxing(start, end) {
+    // TODO: complete me!
+}
+
+// ****************************************************************************
+// PART B: Fanum tax (evasion?)
+// ****************************************************************************
+export function sigmaGrindset(start, end, costs) {
+    // TODO: complete me!
+}
+
+// ****************************************************************************
+// PART C: The economy is cooked
+// ****************************************************************************
+export function cappedMogging(start, end, costs, limits) {
+    // TODO: complete me!
+}
