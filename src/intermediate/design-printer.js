275a987303b3d72a2ae775c214e6fd98f71ae846
Citzn <145261922+Citzn@users.noreply.github.com>
2024-09-27 23:16:53 +1000

    Update design-printer.js

--- src/intermediate/design-printer.js
+++ src/intermediate/design-printer.js
@@ -2,3 +2,20 @@ export function design_printer(designs_list) {
 	
 
 }
+
+function print_bat() {
+	
+}
+
+function print_candy() {
+	
+}
+
+function print_pumpkin() {
+	
+}
+
+
+function print_hat() {
+	
+}
